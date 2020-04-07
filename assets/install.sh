3ea0edb2c0fbb9a4d71d312ac61bd25dccb82657
LGnap <lgnap+git@helpcomputer.org>
2020-04-07 09:51:55 +0200

    refactor: extract opendkim

--- assets/install.sh
+++ assets/install.sh
@@ -16,6 +16,69 @@ enable_tls () {
   fi
 }
 
+opendkim () {
+  if [[ -z "$(find /etc/opendkim/domainkeys -iname *.private)" ]]; then
+    exit 0
+  fi
+  cat >> /etc/supervisor/conf.d/supervisord.conf <<EOF
+    [program:opendkim]
+    command=/usr/sbin/opendkim -f
+EOF
+
+  # /etc/postfix/main.cf
+  postconf -e milter_protocol=2
+  postconf -e milter_default_action=accept
+  postconf -e smtpd_milters=inet:localhost:12301
+  postconf -e non_smtpd_milters=inet:localhost:12301
+
+  cat >> /etc/opendkim.conf <<EOF
+  AutoRestart             Yes
+  AutoRestartRate         10/1h
+  UMask                   002
+  Syslog                  yes
+  SyslogSuccess           Yes
+  LogWhy                  Yes
+
+  Canonicalization        relaxed/simple
+
+  ExternalIgnoreList      refile:/etc/opendkim/TrustedHosts
+  InternalHosts           refile:/etc/opendkim/TrustedHosts
+  KeyTable                refile:/etc/opendkim/KeyTable
+  SigningTable            refile:/etc/opendkim/SigningTable
+
+  Mode                    sv
+  PidFile                 /var/run/opendkim/opendkim.pid
+  SignatureAlgorithm      rsa-sha256
+
+  UserID                  opendkim:opendkim
+
+  Socket                  inet:12301@localhost
+EOF
+
+  cat >> /etc/default/opendkim <<EOF
+  SOCKET="inet:12301@localhost"
+EOF
+
+  cat >> /etc/opendkim/TrustedHosts <<EOF
+  127.0.0.1
+  localhost
+  192.168.0.1/24
+
+  *.$maildomain
+EOF
+
+  cat >> /etc/opendkim/KeyTable <<EOF
+  mail._domainkey.$maildomain $maildomain:mail:$(find /etc/opendkim/domainkeys -iname *.private)
+EOF
+
+  cat >> /etc/opendkim/SigningTable <<EOF
+  *@$maildomain mail._domainkey.$maildomain
+EOF
+
+  chown opendkim:opendkim $(find /etc/opendkim/domainkeys -iname *.private)
+  chmod 400 $(find /etc/opendkim/domainkeys -iname *.private)
+}
+
 #judgement
 if [[ -a /etc/supervisor/conf.d/supervisord.conf ]]; then
   exit 0
@@ -75,60 +138,4 @@ enable_tls
 #############
 #  opendkim
 #############
-
-if [[ -z "$(find /etc/opendkim/domainkeys -iname *.private)" ]]; then
-  exit 0
-fi
-cat >> /etc/supervisor/conf.d/supervisord.conf <<EOF
-
-[program:opendkim]
-command=/usr/sbin/opendkim -f
-EOF
-# /etc/postfix/main.cf
-postconf -e milter_protocol=2
-postconf -e milter_default_action=accept
-postconf -e smtpd_milters=inet:localhost:12301
-postconf -e non_smtpd_milters=inet:localhost:12301
-
-cat >> /etc/opendkim.conf <<EOF
-AutoRestart             Yes
-AutoRestartRate         10/1h
-UMask                   002
-Syslog                  yes
-SyslogSuccess           Yes
-LogWhy                  Yes
-
-Canonicalization        relaxed/simple
-
-ExternalIgnoreList      refile:/etc/opendkim/TrustedHosts
-InternalHosts           refile:/etc/opendkim/TrustedHosts
-KeyTable                refile:/etc/opendkim/KeyTable
-SigningTable            refile:/etc/opendkim/SigningTable
-
-Mode                    sv
-PidFile                 /var/run/opendkim/opendkim.pid
-SignatureAlgorithm      rsa-sha256
-
-UserID                  opendkim:opendkim
-
-Socket                  inet:12301@localhost
-EOF
-cat >> /etc/default/opendkim <<EOF
-SOCKET="inet:12301@localhost"
-EOF
-
-cat >> /etc/opendkim/TrustedHosts <<EOF
-127.0.0.1
-localhost
-192.168.0.1/24
-
-*.$maildomain
-EOF
-cat >> /etc/opendkim/KeyTable <<EOF
-mail._domainkey.$maildomain $maildomain:mail:$(find /etc/opendkim/domainkeys -iname *.private)
-EOF
-cat >> /etc/opendkim/SigningTable <<EOF
-*@$maildomain mail._domainkey.$maildomain
-EOF
-chown opendkim:opendkim $(find /etc/opendkim/domainkeys -iname *.private)
-chmod 400 $(find /etc/opendkim/domainkeys -iname *.private)
+opendkim
