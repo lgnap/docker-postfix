3c9c1fc981653f3ef0df494589d519752acd30ff
LGnap <lgnap+git@helpcomputer.org>
2020-04-07 09:51:00 +0200

    refactor: extract enable_tls

--- assets/install.sh
+++ assets/install.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+enable_tls () {
+  if [[ -n "$(find /etc/postfix/certs -iname *.crt)" && -n "$(find /etc/postfix/certs -iname *.key)" ]]; then
+    # /etc/postfix/main.cf
+    postconf -e smtpd_tls_cert_file=$(find /etc/postfix/certs -iname *.crt)
+    postconf -e smtpd_tls_key_file=$(find /etc/postfix/certs -iname *.key)
+    chmod 400 /etc/postfix/certs/*.*
+    # /etc/postfix/master.cf
+    postconf -M submission/inet="submission   inet   n   -   n   -   -   smtpd"
+    postconf -P "submission/inet/syslog_name=postfix/submission"
+    postconf -P "submission/inet/smtpd_tls_security_level=encrypt"
+    postconf -P "submission/inet/smtpd_sasl_auth_enable=yes"
+    postconf -P "submission/inet/milter_macro_daemon_name=ORIGINATING"
+    postconf -P "submission/inet/smtpd_recipient_restrictions=permit_sasl_authenticated,reject_unauth_destination"
+  fi
+}
+
 #judgement
 if [[ -a /etc/supervisor/conf.d/supervisord.conf ]]; then
   exit 0
@@ -54,19 +70,7 @@ chown postfix.sasl /etc/sasldb2
 ############
 # Enable TLS
 ############
-if [[ -n "$(find /etc/postfix/certs -iname *.crt)" && -n "$(find /etc/postfix/certs -iname *.key)" ]]; then
-  # /etc/postfix/main.cf
-  postconf -e smtpd_tls_cert_file=$(find /etc/postfix/certs -iname *.crt)
-  postconf -e smtpd_tls_key_file=$(find /etc/postfix/certs -iname *.key)
-  chmod 400 /etc/postfix/certs/*.*
-  # /etc/postfix/master.cf
-  postconf -M submission/inet="submission   inet   n   -   n   -   -   smtpd"
-  postconf -P "submission/inet/syslog_name=postfix/submission"
-  postconf -P "submission/inet/smtpd_tls_security_level=encrypt"
-  postconf -P "submission/inet/smtpd_sasl_auth_enable=yes"
-  postconf -P "submission/inet/milter_macro_daemon_name=ORIGINATING"
-  postconf -P "submission/inet/smtpd_recipient_restrictions=permit_sasl_authenticated,reject_unauth_destination"
-fi
+enable_tls
 
 #############
 #  opendkim
