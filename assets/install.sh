b9d5ab97a747f7540fd20a6d63a92f75a3a36656
LGnap <lgnap+git@helpcomputer.org>
2020-04-07 21:42:01 +0200

    fix: config files at the first char of line

--- assets/install.sh
+++ assets/install.sh
@@ -43,8 +43,9 @@ opendkim () {
     return
   fi
   cat >> /etc/supervisor/conf.d/supervisord.conf <<EOF
-    [program:opendkim]
-    command=/usr/sbin/opendkim -f
+[program:opendkim]
+command=/usr/sbin/opendkim -f
+
 EOF
 
   # /etc/postfix/main.cf
@@ -53,48 +54,48 @@ EOF
   postconf -e smtpd_milters=inet:localhost:12301
   postconf -e non_smtpd_milters=inet:localhost:12301
 
-  cat >> /etc/opendkim.conf <<EOF
-  AutoRestart             Yes
-  AutoRestartRate         10/1h
-  UMask                   002
-  Syslog                  yes
-  SyslogSuccess           Yes
-  LogWhy                  Yes
+  cat > /etc/opendkim.conf <<EOF
+AutoRestart             Yes
+AutoRestartRate         10/1h
+UMask                   002
+Syslog                  yes
+SyslogSuccess           Yes
+LogWhy                  Yes
 
-  Canonicalization        relaxed/simple
+Canonicalization        relaxed/simple
 
-  ExternalIgnoreList      refile:/etc/opendkim/TrustedHosts
-  InternalHosts           refile:/etc/opendkim/TrustedHosts
-  KeyTable                refile:/etc/opendkim/KeyTable
-  SigningTable            refile:/etc/opendkim/SigningTable
+ExternalIgnoreList      refile:/etc/opendkim/TrustedHosts
+InternalHosts           refile:/etc/opendkim/TrustedHosts
+KeyTable                refile:/etc/opendkim/KeyTable
+SigningTable            refile:/etc/opendkim/SigningTable
 
-  Mode                    sv
-  PidFile                 /var/run/opendkim/opendkim.pid
-  SignatureAlgorithm      rsa-sha256
+Mode                    sv
+PidFile                 /var/run/opendkim/opendkim.pid
+SignatureAlgorithm      rsa-sha256
 
-  UserID                  opendkim:opendkim
+UserID                  opendkim:opendkim
 
-  Socket                  inet:12301@localhost
+Socket                  inet:12301@localhost
 EOF
 
   cat >> /etc/default/opendkim <<EOF
   SOCKET="inet:12301@localhost"
 EOF
 
-  cat >> /etc/opendkim/TrustedHosts <<EOF
-  127.0.0.1
-  localhost
-  192.168.0.1/24
+  cat > /etc/opendkim/TrustedHosts <<EOF
+127.0.0.1
+localhost
+192.168.0.1/24
 
-  *.$maildomain
+*.$maildomain
 EOF
 
-  cat >> /etc/opendkim/KeyTable <<EOF
-  mail._domainkey.$maildomain $maildomain:mail:$(find /etc/opendkim/domainkeys -iname *.private)
+  cat > /etc/opendkim/KeyTable <<EOF
+mail._domainkey.$maildomain $maildomain:mail:$(find /etc/opendkim/domainkeys -iname *.private)
 EOF
 
-  cat >> /etc/opendkim/SigningTable <<EOF
-  *@$maildomain mail._domainkey.$maildomain
+  cat > /etc/opendkim/SigningTable <<EOF
+*@$maildomain mail._domainkey.$maildomain
 EOF
 
   chown opendkim:opendkim $(find /etc/opendkim/domainkeys -iname *.private)
@@ -117,6 +118,7 @@ command=/opt/postfix.sh
 
 [program:rsyslog]
 command=/usr/sbin/rsyslogd -n -c3
+
 EOF
 
 ############
